6f7a6c8f5202e46060c0ad7a561e7a1b2425b658
litim smail <litimsmail@gmail.com>
2024-10-22 15:57:12 +0100

    final task

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -2,6 +2,34 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
     public void printDays(int year, int month) {
+        int Days = 28;
+        if((year % 4 == 0 && year % 100 != 0) || year % 400 == 0 ){
+            Days = 29;
+        }
+        switch (month){
+            case (1):
+            case (3):
+            case (5):
+            case (7):
+            case (8):
+            case (10):
+            case (12):
+                System.out.println(31);
+                break;
+            case (4):
+            case (6):
+            case (9):
+            case (11):
+                System.out.println(30);
+                break;
+            case (2):
+                System.out.println(Days);
+                break;
+            default:
+                System.out.println("invalid date");
+                break;
+
+        }
 
     }
 }
